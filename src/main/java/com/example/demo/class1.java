84fd84b6d4126668ce39faac0856fff27400be9b
varshini550 <23mh1a0510@acoe.edu.in>
2026-02-24 23:01:58 +0530

    done

---
+++ src/main/java/com/example/demo/class1.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class class1 {
+	@GetMapping("/home")
+	public String disk() {
+		return "welcome to cse";
+	}
+	@GetMapping("/cse")
+	public String disk2() {
+		return "welcome to cse";
+	}
+
+}
