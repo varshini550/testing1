aa821e910fae59ca51ee0f3cba468bdc34e7f394
varshini550 <23mh1a0510@acoe.edu.in>
2026-03-04 10:57:31 +0530

    commit

---
+++ src/main/java/com/example/demo/controller.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class controller {
+	@GetMapping("/cseadd")
+	public int addCSE(@RequestParam int a, @RequestParam int b) {
+		return a + b;
+	}
+
+}
